11f99863ed415274c573a654fa1646fb99c1e775
Harper <r_harper3@tss-hr420-dm12.fc.ca>
2019-03-11 16:56:46 -0400

    added lightbox

---
+++ js/GOT_master.js
@@ -0,0 +1,21 @@
+(() => {
+	console.log("fired");
+
+	// variable stack
+	// grab the sheilds at the bottom of the page
+	const 	shields	 = document.querySelectorAll(".sigil-container"),
+			lightBox = document.querySelector(".lightbox"),
+			video	 = document.querySelector("video");
+
+	function showLightbox() {
+		lightBox.classList.add("show-lightbox");
+	}
+
+		function hideLightbox() {
+		lightBox.classList.remove("show-lightbox");
+	}
+
+	shields.forEach(shield => shield.addEventListener("click", showLightbox));
+
+	video.addEventListener("ended", hideLightbox);
+})();
